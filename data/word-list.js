07280aeb9aceb7e5a312591123a4378d25e332fe
Dan <145234317+finn1817@users.noreply.github.com>
2025-03-31 18:31:52 -0400

    Creating the word-list.js

---
+++ data/word-list.js
@@ -0,0 +1,45 @@
+const wordLists = {
+    easy: [
+        "the", "be", "to", "of", "and", "a", "in", "that", "have", "I", 
+        "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", 
+        "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", 
+        "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", 
+        "so", "up", "out", "if", "about", "who", "get", "which", "go", "me"
+    ],
+    medium: [
+        "about", "better", "bring", "carry", "clean", "drink", "eight", "every", "fight", "first", 
+        "great", "house", "learn", "never", "other", "place", "right", "small", "sound", "spell", 
+        "still", "study", "their", "there", "these", "thing", "think", "three", "water", "where", 
+        "which", "world", "would", "write", "above", "again", "begin", "below", "build", "carry", 
+        "catch", "climb", "close", "count", "cross", "dance", "dream", "drink", "earth", "enjoy"
+    ],
+    hard: [
+        "absolutely", "accessible", "accomplish", "achievement", "acknowledge", "acquisition", "administration", "advertisement", "agricultural", "alternative", 
+        "anniversary", "appreciate", "architecture", "association", "atmosphere", "background", "battlefield", "beautiful", "beginning", "beneficial", 
+        "capability", "certificate", "challenging", "characteristic", "collaboration", "combination", "comfortable", "communicate", "competition", "complicated", 
+        "composition", "comprehensive", "concentration", "conclusion", "conditional", "conference", "confidence", "confirmation", "connection", "consequence", 
+        "conservation", "considerable", "consideration", "consistently", "conspiracy", "contemporary", "contribution", "controversial", "conversation", "cooperation"
+    ],
+    expert: [
+        "acknowledgement", "administration", "advantageous", "advertisement", "aesthetically", "agricultural", "anthropology", "approximately", "autobiography", "bacteriological", 
+        "bibliographical", "biotechnology", "cardiovascular", "characteristic", "chronological", "circumstances", "classification", "collaboration", "communication", "comprehensive", 
+        "computational", "configuration", "consciousness", "constitutional", "controversial", "correspondence", "cryptocurrency", "crystallization", "demonstration", "differentiation", 
+        "disappointment", "discrimination", "distinguishable", "documentation", "effectiveness", "electromagnetic", "embarrassment", "entrepreneurial", "environmentalist", "epidemiological", 
+        "establishment", "extraordinary", "extraterrestrial", "fundamentalism", "generalization", "heterogeneous", "identification", "implementation", "impressionistic", "incomprehensible"
+    ],
+    code: [
+        "function", "variable", "const", "let", "class", "method", "object", "array", "string", "number", 
+        "boolean", "null", "undefined", "async", "await", "promise", "callback", "parameter", "argument", "return", 
+        "import", "export", "module", "component", "props", "state", "render", "effect", "context", "reducer", 
+        "action", "dispatch", "store", "middleware", "selector", "hook", "ref", "memo", "fragment", "element", 
+        "attribute", "event", "listener", "handler", "interface", "type", "generic", "extends", "implements", "instance"
+    ]
+};
+
+const punctuationList = [
+    ".", ",", "!", "?", ";", ":", "'", "\"", "-", "(", ")", "[", "]", "{", "}"
+];
+
+const numbersList = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+];
